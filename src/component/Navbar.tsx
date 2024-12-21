d80b77a468875876dbe2cb17e105c72764a9e9e3
Asangiime <sandeep-ps20292@stu.kln.ac.lk>
2024-12-21 19:59:07 +0530

    Update Navbar.tsx

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link if using React Router
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
-const Header : React.FC = () => {
-  return (
-    <header>
-      <h1>Meal Planner</h1>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/">Meals</Link></li>
-          <li><Link to="/">Recipe</Link></li>
-          <li><Link to="/">Sign In</Link></li>
-         
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Navbar = () => {
+ 
 
-export default Header;
+export default Navbar;
