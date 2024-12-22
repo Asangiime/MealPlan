10b60bf62cbfe6e54413e1f7200232876d92f096
Asangiime <sandeep-ps20292@stu.kln.ac.lk>
2024-12-21 19:38:02 -0800

    Add files via upload

---
+++ pages/vegan.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './vegan.css';  
+const Vegan : React.FC = () => {
+  return (
+    <div>
+      <h2>Vegan Menu</h2>
+      <p>Explore our delicious vegan meal options.</p>
+      {/* Add more vegan meal details here */}
+    </div>
+  );
+};
+
+export default Vegan;
