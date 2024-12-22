86104a6796c3eac86134d46088fb4ddf0f25e9dd
Asangiime <sandeep-ps20292@stu.kln.ac.lk>
2024-12-22 13:48:13 +0530

    change pecatrian.tsx

--- pages/pescatarian.tsx
+++ pages/pescatarian.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import './Pescatarian.css';
+import './pescatarian.css';
 
 const Pescatarian = () => {
+  const recipes = [
+    { id: 1, name: " Pasta" },
+    { id: 2, name: " Sandwich" },
+    { id: 3, name: "Vegetable Stir Fry" },
+  ];
+
   return (
     <div className="pescatarian">
-      <h2>Pescatarian Recipes</h2>
-      <p>Discover amazing pescatarian meal ideas!</p>
+      <h2>pescatarian Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
