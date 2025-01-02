bb5c5658900ffda5d64d575db826aebc4906b393
sandu456 <sadupamasri456@gmail.com>
2025-01-02 03:01:49 -0800

    update db connection

--- mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/MealsdeliveryApplication.java
+++ mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/MealsdeliveryApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MealsdeliveryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MealsdeliveryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MealsdeliveryApplication.class, args);
+    }
 }
