3065c69fef826a7e7e0d4195bbbf40519014101b
sandu456 <sadupamasri456@gmail.com>
2025-01-03 19:38:02 -0800

    added update order option

--- mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/Controllers/OrderController.java
+++ mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/Controllers/OrderController.java
@@ -1,28 +1,45 @@
 package com.example.mealsdelivery.Controllers;
 
+import com.example.mealsdelivery.Service.OrderService;
+import com.example.mealsdelivery.models.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
 
-    @PostMapping
-    public String createOrder(@RequestBody String order) {
-        // Logic for creating an order
-        return "Order created";
+    @PostMapping("/create")
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
     }
 
     @GetMapping("/{id}")
-    public String getOrderDetails(@PathVariable String id) {
-        // Logic for retrieving order details
-        return "Order details for " + id;
+    public Optional<Order> getOrderById(@PathVariable String id) {
+        return orderService.getOrderById(id);
     }
 
-    @PostMapping("/{id}/pay")
-    public String processPayment(@PathVariable String id) {
-        // Logic for processing payment
-        return "Payment processed for order " + id;
+    @GetMapping("/{id}")
+    public List<Order> getOrdersByCustomerId(@PathVariable String id) {
+        return orderService.getOrdersByCustomerId(id);
     }
+
+    @GetMapping("/get")
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PutMapping("/{id}")
+    public Order updateOrder(@PathVariable String id, @RequestBody Order order){
+        return orderService.updateOrder(id,order);
+    }
+
+
 }
 
 
